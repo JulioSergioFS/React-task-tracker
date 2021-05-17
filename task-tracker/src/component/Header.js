9345e531d51fd288a0c2093da02a316d32adfef4
Julio Sergio <juliosergio010302@gmail.com>
2021-05-17 15:45:07 -0300

    Created an event for the Add button
    the event starts when you click the Add button

--- task-tracker/src/component/Header.js
+++ task-tracker/src/component/Header.js
@@ -2,10 +2,14 @@ import PropTypes from 'prop-types';
 import Button from './Button.js';
 
 const Header = ({ title }) => {
+  const onClick = () => {
+    console.log('boop.')
+  }
+
     return (
         <header className='header'>
             <h1>{title}</h1>
-            <Button color='green' text='Add' />
+            <Button color='green' text='Add' onClick={onClick} />
         </header>
     )
 }
