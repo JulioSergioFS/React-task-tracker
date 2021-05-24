55482c20eb55224b1abb7ed955f7f3930d6f7b58
Julio Sergio <juliosergio010302@gmail.com>
2021-05-24 10:33:09 -0300

    Made the component global
    Put the component in the App.js file to make it global.

--- task-tracker/src/App.js
+++ task-tracker/src/App.js
@@ -1,12 +1,34 @@
+import { useState } from 'react'
 import './App.css';
 import Header from './component/Header.js';
 import Task from './component/Tasks.js';
 
 function App() {
+  const [tasks, setTask] = useState([
+    {
+      id: 1,
+      text: 'Doctors Appointemnt',
+      day: 'Feb 5th at 2:30pm',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Meeting at School',
+      day: 'Feb 6th at 1:30pm',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'Food Shopping',
+      day: 'Feb 5th at 2:30pm',
+      reminder: false,
+    }
+  ])
+
   return (
     <div className="container">
       <Header />
-      <Task />
+      <Task tasks={tasks} />
     </div>
   );
 }
